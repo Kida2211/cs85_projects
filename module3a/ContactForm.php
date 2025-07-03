f6680d7a308eb24e907bf65e1c7ac5d816399de8
Nathan Kida <nathan.kida@gmail.com>
2025-07-02 17:39:24 -0700

    Add input and email validation functions with error handling

--- module3a/ContactForm.php
+++ module3a/ContactForm.php
@@ -6,7 +6,38 @@
 <body>
 
 <?php
-// Placeholder for PHP logic
+
+// Function to validate and sanitize general input
+function validateInput($data, $fieldName) {
+    global $errorCount;
+    if (empty($data)) {
+        echo "\"$fieldName\" is a required field.<br />\n";
+        ++$errorCount;
+        $retval = "";
+    } else {
+        $retval = trim($data); // Remove surrounding whitespace
+        $retval = stripslashes($retval); // Remove backslashes
+    }
+    return($retval);
+}
+
+// Function to validate and sanitize email input
+function validateEmail($data, $fieldName) {
+    global $errorCount;
+    if (empty($data)) {
+        echo "\"$fieldName\" is a required field.<br />\n";
+        ++$errorCount;
+        $retval = "";
+    } else {
+        $retval = filter_var($data, FILTER_SANITIZE_EMAIL); // Remove unwanted characters
+        if (!filter_var($retval, FILTER_VALIDATE_EMAIL)) {
+            echo "\"$fieldName\" is not a valid e-mail address.<br />\n";
+            ++$errorCount;
+        }
+    }
+    return($retval);
+}
+
 ?>
 
 </body>
